64486be25662ac0fbbb71cb78e8e7649eddd93db
arizon-supersu <0345iarizon@e-itaca.es>
2022-02-21 19:06:56 +0100

    Añadidas pantallas principales

--- app/src/main/java/com/example/proyectofinalgrupal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinalgrupal/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.proyectofinalgrupal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Esto es una prueba de commit
+    EditText email;
+    EditText contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email.findViewById(R.id.CampoEmail);
+        contraseña.findViewById(R.id.CampoContraseña);
+
     }
 }
