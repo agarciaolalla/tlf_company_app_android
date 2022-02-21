59a102aaf44abf8f69d40e369870387b490f3bfd
Olallak <olallak98@gmail.com>
2022-02-21 18:50:37 +0100

    Probando COMMIT

--- app/src/main/java/com/example/proyectofinalgrupal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinalgrupal/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Esto es una prueba de commit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
