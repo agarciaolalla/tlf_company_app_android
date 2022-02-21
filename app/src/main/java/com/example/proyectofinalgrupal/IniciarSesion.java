00867d13dddb5c07aad3bec175858a49f3dfd2f9
Olallak <olallak98@gmail.com>
2022-02-21 22:24:20 +0100

    Añadida pantalla de registro y botones de inicio sesion / registro en la pantalla de incio sesion

--- app/src/main/java/com/example/proyectofinalgrupal/IniciarSesion.java
+++ app/src/main/java/com/example/proyectofinalgrupal/IniciarSesion.java
@@ -3,17 +3,22 @@ package com.example.proyectofinalgrupal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class IniciarSesion extends AppCompatActivity {
 
     EditText email;
     EditText contraseña;
+    Button registrar = findViewById(R.id.registrar);
+    Button iSesion = findViewById(R.id.iniciarSesion);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.iniciar_sesion);
         email.findViewById(R.id.CampoEmail);
         contraseña.findViewById(R.id.CampoContraseña);
 
---
+++ app/src/main/java/com/example/proyectofinalgrupal/Registro.java
@@ -0,0 +1,4 @@
+package com.example.proyectofinalgrupal;
+
+public class Registro {
+}
